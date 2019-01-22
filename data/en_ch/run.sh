a890c5d797b12591e74a87436e826141bba5a49d
qianchu <hey_flora@126.com>
2019-01-22 17:29:52 +0000

    pipeline

--- data/en_ch/run.sh
+++ data/en_ch/run.sh
@@ -1,4 +1,8 @@
-python3 convert2tra.py $2
-python3 preprocess.py $1 ch_tra.txt $3 $4
+en_txt=$1
+ch_txt=$2
+en_vocab_limit=$3
+ch_vocab_limit=$4
+
+python3 preprocess.py $en_txt $ch_txt $en_vocab_limit $ch_vocab_limit
 python3 bi_make_sensplit_test.py
 python3 make_sensplit_test_general.py en_vocab ratings.txt en_test.txt scws_en.txt
