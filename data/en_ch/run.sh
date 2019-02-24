c25cda3db863c5c4801d66f3204fec060e3a5b7a
qianchu <hey_flora@126.com>
2019-02-24 19:16:20 +0000

    python

--- data/en_ch/run.sh
+++ data/en_ch/run.sh
@@ -4,12 +4,12 @@ en_vocab_limit=$3
 ch_vocab_limit=$4
 
 echo python3 convert2tra.py $ch_txt
-python3 convert2tra.py $ch_txt
+python convert2tra.py $ch_txt
 echo python3 tokenize_english.py $en_txt
-python3 tokenize_english.py $en_txt
+python tokenize_english.py $en_txt
 echo python3 preprocess.py $en_txt ch_tra.txt $en_vocab_limit $ch_vocab_limit
-python3 preprocess.py $en_txt ch_tra.txt $en_vocab_limit $ch_vocab_limit
+python preprocess.py $en_txt ch_tra.txt $en_vocab_limit $ch_vocab_limit
 echo python3 bi_make_sensplit_test.py
-python3 bi_make_sensplit_test.py
+python bi_make_sensplit_test.py
 echo python3 make_sensplit_test_general.py en_vocab ratings.txt en_test.txt scws_en.txt
-python3 make_sensplit_test_general.py en_vocab ratings.txt en_test.txt scws_en.txt
+python make_sensplit_test_general.py en_vocab ratings.txt en_test.txt scws_en.txt
